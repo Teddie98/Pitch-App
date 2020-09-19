51b6f2034219b457bf7e3dc7ae430af34ce7c66a
Teddie <teddymachoka98@yahoo.com>
2020-09-19 17:33:38 +0300

    added the migrations files and folders

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,20 +1,26 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField,TextAreaField,SubmitField, SelectField, RadioField
+from wtforms import StringField, TextAreaField, SubmitField, SelectField, RadioField
 from wtforms.validators import Required
+
 
 class CommentsForm(FlaskForm):
     comment = TextAreaField('Comment', validators=[Required()])
-    vote=RadioField('default field arguments', choices=[('1', 'UpVote'), ('1', 'DownVote')])
-    submit = SubmitField('SUBMIT')  
+    vote = RadioField('default field arguments', choices=[
+                      ('1', 'UpVote'), ('1', 'DownVote')])
+    submit = SubmitField('SUBMIT')
+
 
 class UpdateProfile(FlaskForm):
-    bio = TextAreaField('Tell us about you.',validators = [Required()])
-    submit = SubmitField('Submit') 
+    bio = TextAreaField('Tell us about you.', validators=[Required()])
+    submit = SubmitField('Submit')
+
 
 class PitchForm(FlaskForm):
-    category_id = SelectField('Select Category', choices=[('1', 'Interview'), ('2', 'Pick Up Lines'), ('3', 'Promotion'),('4','Product')])
+    category_id = SelectField('Select Category', choices=[(
+        '1', 'Interview'), ('2', 'Pick Up Lines'), ('3', 'Promotion'), ('4', 'Product')])
     content = TextAreaField('YOUR PITCH')
     submit = SubmitField('Create Pitch')
+
 
 class UpvoteForm(FlaskForm):
     '''
--- app/main/views.py
+++ app/main/views.py
@@ -1,12 +1,11 @@
-from flask import render_template, request, redirect, url_for, abort  
-from . import main  
+from flask import render_template, request, redirect, url_for, abort
+from . import main
 from .forms import CommentsForm, UpdateProfile, PitchForm, UpvoteForm
-from ..models import Comment, Pitch, User 
+from ..models import comment, Pitch, User
 from flask_login import login_required, current_user
-from .. import db,photos
+from .. import db, photos
 
 import markdown2
-
 
 
 @main.route('/')
@@ -17,20 +16,22 @@ def index():
     title = 'Home - Welcome to The best Pitching Website Online'
 
     search_pitch = request.args.get('pitch_query')
-    pitches= Pitch.get_all_pitches()  
+    pitches = Pitch.get_all_pitches()
 
-    return render_template('index.html', title = title, pitches= pitches)
+    return render_template('index.html', title=title, pitches=pitches)
 
-#this section consist of the category root functions
+# this section consist of the category root functions
+
 
 @main.route('/inteview/pitches/')
 def interview():
     '''
     View root page function that returns the index page and its data
     '''
-    pitches= Pitch.get_all_pitches()
-    title = 'Home - Welcome to The best Pitching Website Online'  
-    return render_template('interview.html', title = title, pitches= pitches )
+    pitches = Pitch.get_all_pitches()
+    title = 'Home - Welcome to The best Pitching Website Online'
+    return render_template('interview.html', title=title, pitches=pitches)
+
 
 @main.route('/pick_up_lines/pitches/')
 def pick_up_line():
@@ -39,9 +40,10 @@ def pick_up_line():
     '''
     title = 'Pick Up Lines'
 
-    pitches= Pitch.get_all_pitches()
+    pitches = Pitch.get_all_pitches()
 
-    return render_template('pick_up_lines.html', title = title, pitches= pitches )
+    return render_template('pick_up_lines.html', title=title, pitches=pitches)
+
 
 @main.route('/promotion/pitches/')
 def promotion():
@@ -50,9 +52,9 @@ def promotion():
     '''
     title = 'Promotion Pitches'
 
-    pitches= Pitch.get_all_pitches()
+    pitches = Pitch.get_all_pitches()
 
-    return render_template('promotion.html', title = title, pitches= pitches )
+    return render_template('promotion.html', title=title, pitches=pitches)
 
 
 @main.route('/product/pitches/')
@@ -61,22 +63,23 @@ def product():
     View root page function that returns the index page and its data
     '''
     title = 'Product Pitches'
-    pitches= Pitch.get_all_pitches()
-    return render_template('product.html', title = title, pitches= pitches )
- 
+    pitches = Pitch.get_all_pitches()
+    return render_template('product.html', title=title, pitches=pitches)
+
 #  end of category root functions
+
 
 @main.route('/pitch/<int:pitch_id>')
 def pitch(pitch_id):
-
     '''
     View pitch page function that returns the pitch details page and its data
     '''
-    found_pitch= get_pitch(pitch_id)
+    found_pitch = get_pitch(pitch_id)
     title = pitch_id
     pitch_comments = Comment.get_comments(pitch_id)
 
-    return render_template('pitch.html',title= title ,found_pitch= found_pitch, pitch_comments= pitch_comments)
+    return render_template('pitch.html', title=title, found_pitch=found_pitch, pitch_comments=pitch_comments)
+
 
 @main.route('/search/<pitch_name>')
 def search(pitch_name):
@@ -86,9 +89,10 @@ def search(pitch_name):
     searched_pitches = search_pitch(pitch_name)
     title = f'search results for {pitch_name}'
 
-    return render_template('search.html',pitches = searched_pitches)
+    return render_template('search.html', pitches=searched_pitches)
 
-@main.route('/pitch/new/', methods = ['GET','POST'])
+
+@main.route('/pitch/new/', methods=['GET', 'POST'])
 @login_required
 def new_pitch():
     '''
@@ -96,19 +100,19 @@ def new_pitch():
     '''
     form = PitchForm()
 
-
     if category is None:
-        abort( 404 )
+        abort(404)
 
     if form.validate_on_submit():
-        pitch= form.content.data
+        pitch = form.content.data
         category_id = form.category_id.data
-        new_pitch= Pitch(pitch= pitch, category_id= category_id)
+        new_pitch = Pitch(pitch=pitch, category_id=category_id)
 
         new_pitch.save_pitch()
         return redirect(url_for('main.index'))
 
-    return render_template('new_pitch.html', new_pitch_form= form, category= category)
+    return render_template('new_pitch.html', new_pitch_form=form, category=category)
+
 
 @main.route('/category/<int:id>')
 def category(id):
@@ -121,44 +125,49 @@ def category(id):
         abort(404)
 
     pitches_in_category = Pitches.get_pitch(id)
-    return render_template('category.html' ,category= category, pitches= pitches_in_category)
+    return render_template('category.html', category=category, pitches=pitches_in_category)
 
-@main.route('/pitch/comments/new/<int:id>',methods = ['GET','POST'])
+
+@main.route('/pitch/comments/new/<int:id>', methods=['GET', 'POST'])
 @login_required
 def new_comment(id):
     form = CommentsForm()
     vote_form = UpvoteForm()
     if form.validate_on_submit():
-        new_comment = Comment(pitch_id =id,comment=form.comment.data,username=current_user.username,votes=form.vote.data)
+        new_comment = Comment(pitch_id=id, comment=form.comment.data,
+                              username=current_user.username, votes=form.vote.data)
         new_comment.save_comment()
         return redirect(url_for('main.index'))
     #title = f'{pitch_result.id} review'
-    return render_template('new_comment.html',comment_form=form, vote_form= vote_form)
+    return render_template('new_comment.html', comment_form=form, vote_form=vote_form)
 
-@main.route('/user/<uname>/update/pic',methods= ['POST'])
+
+@main.route('/user/<uname>/update/pic', methods=['POST'])
 @login_required
 def update_pic(uname):
-    user = User.query.filter_by(username = uname).first()
+    user = User.query.filter_by(username=uname).first()
     if 'photo' in request.files:
         filename = photos.save(request.files['photo'])
         path = f'photos/{filename}'
-        user.profile_pic_path = path 
+        user.profile_pic_path = path
         db.session.commit()
-    return redirect(url_for('main.profile',uname=uname))
+    return redirect(url_for('main.profile', uname=uname))
+
 
 @main.route('/user/<uname>')
 def profile(uname):
-    user = User.query.filter_by(username = uname).first()
+    user = User.query.filter_by(username=uname).first()
 
     if user is None:
         abort(404)
 
-    return render_template("profile/profile.html", user = user)
+    return render_template("profile/profile.html", user=user)
 
-@main.route('/user/<uname>/update',methods = ['GET','POST'])
+
+@main.route('/user/<uname>/update', methods=['GET', 'POST'])
 @login_required
 def update_profile(uname):
-    user = User.query.filter_by(username = uname).first()
+    user = User.query.filter_by(username=uname).first()
     if user is None:
         abort(404)
 
@@ -170,9 +179,10 @@ def update_profile(uname):
         db.session.add(user)
         db.session.commit()
 
-        return redirect(url_for('.profile',uname=user.username))
-    
-    return render_template('profile/update.html',form =form)
+        return redirect(url_for('.profile', uname=user.username))
+
+    return render_template('profile/update.html', form=form)
+
 
 @main.route('/view/comment/<int:id>')
 def view_comments(id):
@@ -180,14 +190,13 @@ def view_comments(id):
     Function that returs  the comments belonging to a particular pitch
     '''
     comments = Comment.get_comments(id)
-    return render_template('view_comments.html',comments = comments, id=id)
+    return render_template('view_comments.html', comments=comments, id=id)
 
 
-
-@main.route('/test/<int:id>')  
+@main.route('/test/<int:id>')
 def test(id):
     '''
     this is route for basic testing
     '''
-    pitch =Pitch.query.filter_by(id=1).first()
-    return render_template('test.html',pitch= pitch)
+    pitch = Pitch.query.filter_by(id=1).first()
+    return render_template('test.html', pitch=pitch)
